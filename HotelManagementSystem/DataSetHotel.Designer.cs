53b63b5119fe009c94f52c731d42d9b9b6cf6767
m-mazur <mazur.marcus@gmail.com>
2014-02-06 13:04:11 +0100

    DataSet Fix

--- HotelManagementSystem/DataSetHotel.Designer.cs
+++ HotelManagementSystem/DataSetHotel.Designer.cs
@@ -3797,11 +3797,21 @@ namespace HotelManagementSystem.DataSetHotelTableAdapters {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.Odbc.OdbcCommand[1];
+            this._commandCollection = new global::System.Data.Odbc.OdbcCommand[2];
             this._commandCollection[0] = new global::System.Data.Odbc.OdbcCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT \"room_no\", \"max_persons\", \"price_per_day\", \"room_type\" FROM \"dbo\".\"rooms\"";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.Odbc.OdbcCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = @"SELECT room_no, max_persons, price_per_day, room_type
+FROM  dbo.rooms
+WHERE (room_no NOT IN
+             (SELECT rooms_1.room_no
+            FROM  dbo.reservations, dbo.rooms rooms_1
+            WHERE dbo.reservations.room_no = rooms_1.room_no AND (dbo.reservations.check_in_date BETWEEN '1/1/2014' AND '2/5/2014') AND (dbo.reservations.check_out_date BETWEEN '1/1/2014' AND '2/5/2014')
+            GROUP BY rooms_1.room_no))";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -3823,6 +3833,30 @@ namespace HotelManagementSystem.DataSetHotelTableAdapters {
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual DataSetHotel.roomsDataTable GetData() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            DataSetHotel.roomsDataTable dataTable = new DataSetHotel.roomsDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillByAvailableRooms(DataSetHotel.roomsDataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual DataSetHotel.roomsDataTable GetDataByAvailableRooms() {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
             DataSetHotel.roomsDataTable dataTable = new DataSetHotel.roomsDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
