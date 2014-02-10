cfacc72a4ee2f3a136aac6c6505adf2fc6481861
m-mazur <mazur.marcus@gmail.com>
2014-02-10 16:53:14 +0100

    Order Util

---
+++ HotelManagementSystem/OrderUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem
+{
+    public class OrderUtility
+    {
+        private ReservationsRepository reservationReposistory;
+
+        public string TotalOrderValue(double roomPrice, string reservationNo)
+        {
+            return (roomPrice * TotalNights(reservationNo)).ToString();
+        }
+
+        public int TotalNights(string reservationNo)
+        {
+            reservationReposistory = new ReservationsRepository();
+            TimeSpan timeSpan = reservationReposistory.GetSingleReservation(reservationNo).CheckOutDate -
+                reservationReposistory.GetSingleReservation(reservationNo).CheckInDate;
+            int totalDays = timeSpan.Days;
+            return totalDays;
+        }
+    }
+}
