a92cb9a86c62e1d4707dcd9ec94b331d070c8d9b
m-mazur <mazur.marcus@gmail.com>
2014-02-11 15:31:13 +0100

    Validate

---
+++ HotelManagementSystem/Validate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem
+{
+    public static class Validate
+    {
+        public static Boolean ValidString(string validate)
+        {
+            if(string.IsNullOrEmpty(validate))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
