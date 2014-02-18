94d83f3e49820304ee16462336ad4dbc1bd7f905
m-mazur <mazur.marcus@gmail.com>
2014-02-18 14:17:30 +0100

    Customer query

--- HotelManagementSystem/Model/Database/DataSetHotel.Designer.cs
+++ HotelManagementSystem/Model/Database/DataSetHotel.Designer.cs
@@ -2909,12 +2909,18 @@ namespace HotelManagementSystem.Model.Database.DataSetHotelTableAdapters {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.Odbc.OdbcCommand[1];
+            this._commandCollection = new global::System.Data.Odbc.OdbcCommand[2];
             this._commandCollection[0] = new global::System.Data.Odbc.OdbcCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT \"e_mail\", \"phone_no\", \"phone_country_code\", \"credit_card_no\", \"first_name\"" +
                 ", \"last_name\" FROM \"dbo\".\"customers\"";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.Odbc.OdbcCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT e_mail, phone_no, phone_country_code, credit_card_no, first_name, last_nam" +
+                "e\r\nFROM  dbo.customers\r\nWHERE (e_mail = ?)";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.Odbc.OdbcParameter("e_mail", global::System.Data.Odbc.OdbcType.VarChar, 1024, global::System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "e_mail", global::System.Data.DataRowVersion.Current, false, null));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -2936,6 +2942,42 @@ namespace HotelManagementSystem.Model.Database.DataSetHotelTableAdapters {
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, true)]
         public virtual DataSetHotel.customersDataTable GetData() {
             this.Adapter.SelectCommand = this.CommandCollection[0];
+            DataSetHotel.customersDataTable dataTable = new DataSetHotel.customersDataTable();
+            this.Adapter.Fill(dataTable);
+            return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillByCustomerEmail(DataSetHotel.customersDataTable dataTable, string e_mail) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((e_mail == null)) {
+                throw new global::System.ArgumentNullException("e_mail");
+            }
+            else {
+                this.Adapter.SelectCommand.Parameters[0].Value = ((string)(e_mail));
+            }
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Select, false)]
+        public virtual DataSetHotel.customersDataTable GetDataByCustomerEmail(string e_mail) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((e_mail == null)) {
+                throw new global::System.ArgumentNullException("e_mail");
+            }
+            else {
+                this.Adapter.SelectCommand.Parameters[0].Value = ((string)(e_mail));
+            }
             DataSetHotel.customersDataTable dataTable = new DataSetHotel.customersDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
